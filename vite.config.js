64f6c1d92c4458e74523257b61e991704b9db79b
Ahmad's Nadeem <asyouknowme119@gmail.com>
2026-05-19 09:15:50 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true // Ye har tarah ke tunnel host ko allow kar dega
-  }
+  base: process.env.VITE_BASE_PATH || "/SMH-Hospital"
+  // server: {
+  //   allowedHosts: true // Ye har tarah ke tunnel host ko allow kar dega
+  // }
 })
